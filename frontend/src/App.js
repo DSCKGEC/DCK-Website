55cc82bdb3dd407d5208510b8a2dc528fe03e7e2
suchana <suchanachakraborty@gmail.com>
2020-02-10 23:53:07 +0530

    modified app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,19 @@
 import React, {Component} from 'react';
-import Navbar from './components/navbar.js';
-import Topbar from './components/topbar.js';
-import AboutUs from './components/about.js';
-import Activity from './components/activities.js';
-import Team from './components/team.js';
-import Cards from './components/cards.js';
-import Partners from './components/partners.js';
-import AboutKgec from './components/about_kgec.js';
-import Footer from './components/footer.js';
-
+import Homepage from './components/homepage.js';
+import Events from './components/events.js';
+import {Router,Route,Switch,Link} from 'react-router';
 
 class App extends Component{
 
   render(){
     return(
-      <body>
-        <Navbar/>
-        <Topbar/>
-        <AboutUs/>
-        <Activity/>
-        <Team/>
-        <Cards/>
-        <Partners/>
-        <AboutKgec/>
-        <Footer/>
+      <body> 
+        <Router>
+        <Switch>
+          <Route path="/"><Homepage /></Route>
+          <Route path="/events"><Events /></Route>
+        </Switch>
+        </Router>
       </body>
     ); 
   }
