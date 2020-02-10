cff10b0b03b7b0239693e4ea58d35103a9a970ec
suchana <suchanachakraborty@gmail.com>
2020-02-10 23:50:33 +0530

    modified navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -5,14 +5,11 @@ class Navbar extends Component{
   render(){
     return(
         <div>
-              <div className="navbar-text-home">HOME</div>
-              <div className="navbar-text-events">EVENTS</div>
-              <div className="navbar-text-team">TEAM</div>
-              <div className="navbar-text-blog">BLOG</div>
             
-            <div className = "img-logo"></div>
-            <div className= "title">dckgec</div>
-            <button className = "sign-up">sign up</button>
+              <div className = "img-logo"></div>
+              <div className= "title">dckgec</div>
+              <button className = "sign-up">sign up</button>
+          
         </div>
     ); 
   }
