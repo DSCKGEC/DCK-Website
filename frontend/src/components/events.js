46b1230ede0aba35e88866c41f542ff7145f9a67
suchana <suchanachakraborty@gmail.com>
2020-02-10 23:51:27 +0530

    added events

---
+++ frontend/src/components/events.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Navbar from './navbar.js';
+
+class Events extends Component{
+    render(){
+        return(
+            <div>
+            <Navbar/>
+            </div>
+        );
+    }
+}
+export default Events;
