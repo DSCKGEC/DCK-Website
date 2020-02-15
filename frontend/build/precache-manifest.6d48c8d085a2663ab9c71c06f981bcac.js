f728133b2d865da5184396fa3cdc9dc1d622f9e6
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2020-02-16 02:04:23 +0530

    frontend build done on Ayushman's Laptop

---
+++ frontend/build/precache-manifest.6d48c8d085a2663ab9c71c06f981bcac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8c4e2ee215cc572c541b98f2713cb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f7cbbe0e7af97a2e28d",
+    "url": "/static/css/main.81a1881a.chunk.css"
+  },
+  {
+    "revision": "7d382c414d9a73c31980",
+    "url": "/static/js/2.dd131fe5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.dd131fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7cbbe0e7af97a2e28d",
+    "url": "/static/js/main.2f6f9b86.chunk.js"
+  },
+  {
+    "revision": "b012bda2c830ab3e037c",
+    "url": "/static/js/runtime-main.4d1a2f59.js"
+  },
+  {
+    "revision": "912c5e29b02f36471afab4d60d403a6e",
+    "url": "/static/media/1552715138phpC3AcrS.912c5e29.png"
+  },
+  {
+    "revision": "17a39ecf8dc2ed8e6fb46e002f49d0ee",
+    "url": "/static/media/69096379_103099181060134_30738475347083264_o.17a39ecf.png"
+  },
+  {
+    "revision": "edfc98137932f7edfdad1b1dd1bc5ea2",
+    "url": "/static/media/76266712_148129396557112_3875172377830621184_o.edfc9813.png"
+  },
+  {
+    "revision": "96a31ff6d81bda0497363896af6f1072",
+    "url": "/static/media/Developers doing discussion about wireframe.96a31ff6.png"
+  },
+  {
+    "revision": "2f6fc3b6b5b83687275ff1f353a1a9a1",
+    "url": "/static/media/Page 1.2f6fc3b6.png"
+  },
+  {
+    "revision": "74412a3bd840870ff6b9bdbef9944193",
+    "url": "/static/media/undraw_shared_workspace_hwky.74412a3b.png"
+  },
+  {
+    "revision": "f3e676e63cf105a2051d7bdb4ceea828",
+    "url": "/static/media/undraw_team_page_pgpr.f3e676e6.png"
+  }
+]);
